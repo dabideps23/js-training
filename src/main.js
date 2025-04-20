76e629299f32d7829b098969ac89d402aafa3ebd
dabideps23 <dabide.ps23@gmail.com>
2025-04-20 12:29:53 +0900

    let変数の挙動確認

--- src/main.js
+++ src/main.js
@@ -11,3 +11,14 @@ console.log(val1);
 //　var変数は再宣言可能
 var val1 = 'var変数を再宣言';
 console.log(val1);
+
+let let1 = 'let変数';
+console.log(let1);
+
+// let変数は上書き可能
+let1 = 'let変数を上書き';
+console.log(let1);
+
+//　let変数は再宣言可能
+// let let1 = 'let変数を再宣言';
+// console.log(let1);
